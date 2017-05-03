8d4b95ad602d53b1d8db85f267b72df8aedc88cb
Andreas Larsson <andreas.larsson@u-blox.com>
2017-05-03 23:07:52 +0200

    Added temp update when connected over BLE

--- source/main.cpp
+++ source/main.cpp
@@ -1,23 +1,102 @@
-#include <mbed.h>
+#include <events/mbed_events.h>
+#include "mbed.h"
+#include "ble/BLE.h"
+#include "ble/services/HealthThermometerService.h"
+
 #include "DS18B20.h"
 #include "OneWireDefs.h"
 #include "nrf_delay.h"
 
+const static char     DEVICE_NAME[]        = "Therm";
+static const uint16_t uuid16_list[]        = {GattService::UUID_HEALTH_THERMOMETER_SERVICE};
+
+static float                     currentTemperature   = 0;
+static HealthThermometerService *thermometerServicePtr;
+
+DS18B20 therm_device(true, false, false, D2);
+
+static EventQueue eventQueue(
+    /* event count */ 16 * /* event size */ 32
+);
+
+/* Restart Advertising on disconnection*/
+void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *)
+{
+    BLE::Instance().gap().startAdvertising();
+}
+
+void updateSensorValue(void) {
+    /* Do blocking calls or whatever is necessary for sensor polling.
+       In our case, we simply update the Temperature measurement. */
+    currentTemperature = therm_device.readTemperature();
+    if (currentTemperature != -999.0000)
+    {
+        printf("Temp=%f\n", currentTemperature);
+
+        thermometerServicePtr->updateTemperature(currentTemperature);
+    }
+}
+
+void periodicCallback(void)
+{
+    if (BLE::Instance().gap().getState().connected) {
+        eventQueue.call(updateSensorValue);
+    }
+}
+
+void onBleInitError(BLE &ble, ble_error_t error)
+{
+   /* Initialization error handling should go here */
+}
+
+void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
+{
+    BLE&        ble   = params->ble;
+    ble_error_t error = params->error;
+
+    if (error != BLE_ERROR_NONE) {
+        onBleInitError(ble, error);
+        return;
+    }
+
+    if (ble.getInstanceID() != BLE::DEFAULT_INSTANCE) {
+        return;
+    }
+
+    ble.gap().onDisconnection(disconnectionCallback);
+
+    /* Setup primary service. */
+    thermometerServicePtr = new HealthThermometerService(ble, currentTemperature, HealthThermometerService::LOCATION_EAR);
+
+    /* setup advertising */
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list));
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::THERMOMETER_EAR);
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
+    ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
+    ble.gap().setAdvertisingInterval(1000); /* 1000ms */
+    ble.gap().startAdvertising();
+}
+
+void scheduleBleEventsProcessing(BLE::OnEventsToProcessCallbackContext* context) {
+    BLE &ble = BLE::Instance();
+    eventQueue.call(Callback<void()>(&ble, &BLE::processEvents));
+}
+
 int main()
 {
-    // device( crcOn, useAddress, parasitic, mbed pin )
-    DS18B20 device(true, false, false, D2);
+    eventQueue.call_every(500, periodicCallback);
+
+    BLE &ble = BLE::Instance();
+    ble.onEventsToProcess(scheduleBleEventsProcessing);
+    ble.init(bleInitComplete);
+
+    while (!therm_device.initialize())
+        ;    // keep calling until it works
     
-    while (!device.initialize());    // keep calling until it works
+    therm_device.setResolution(twelveBit);
     
-    while (true)
-    {
-        float temp;
-        device.setResolution(twelveBit);
-        temp = device.readTemperature(); 
-        printf("Temp=%f\n", temp);
-        wait(1);
-    }
-     
-    return EXIT_SUCCESS;
+    eventQueue.dispatch_forever();
+
+    return 0;
 }
