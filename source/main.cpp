bc8ebe3d0037c353b3cb40bcf0d5cd50ee8e10c3
Andreas Larsson <andreas.larsson@u-blox.com>
2017-05-03 22:37:55 +0200

    Replaced with another OneWire/Thermometer driver

--- source/main.cpp
+++ source/main.cpp
@@ -1,106 +1,62 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2006-2013 ARM Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+* OneWireCRC/OneWireThermometer demo.
+*
+* Copyright (C) <2009> Petras Saduikis <petras@petras.co.uk>
+*
+* This file is part of OneWireCRC/OneWireThermometer.
+*
+* OneWireCRC/OneWireThermometer is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* OneWireCRC/OneWireThermometer is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with OneWireCRC/OneWireThermometer0.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
-#include <events/mbed_events.h>
-#include "mbed.h"
-#include "ble/BLE.h"
-#include "ble/services/HealthThermometerService.h"
+////////////////////////////////////////////////////////////////////
+// Test code to read temperature from a Maxim DS18B20 or DS18S20
+// 1-wire device 
+////////////////////////////////////////////////////////////////////
 
-DigitalOut led1(LED1, 1);
+#include <mbed.h>
+#include "DS18S20.h"
+#include "DS18B20.h"
+#include "OneWireDefs.h"
+#include "nrf_delay.h"
 
-const static char     DEVICE_NAME[]        = "Therm";
-static const uint16_t uuid16_list[]        = {GattService::UUID_HEALTH_THERMOMETER_SERVICE};
-
-static float                     currentTemperature   = 39.6;
-static HealthThermometerService *thermometerServicePtr;
-
-static EventQueue eventQueue(
-    /* event count */ 16 * /* event size */ 32
-);
-
-/* Restart Advertising on disconnection*/
-void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *)
-{
-    BLE::Instance().gap().startAdvertising();
-}
-
-void updateSensorValue(void) {
-    /* Do blocking calls or whatever is necessary for sensor polling.
-       In our case, we simply update the Temperature measurement. */
-    currentTemperature = (currentTemperature + 0.1 > 43.0) ? 39.6 : currentTemperature + 0.1;
-    thermometerServicePtr->updateTemperature(currentTemperature);
-}
-
-void periodicCallback(void)
-{
-    led1 = !led1; /* Do blinky on LED1 while we're waiting for BLE events */
-
-    if (BLE::Instance().gap().getState().connected) {
-        eventQueue.call(updateSensorValue);
-    }
-}
-
-void onBleInitError(BLE &ble, ble_error_t error)
-{
-   /* Initialization error handling should go here */
-}
-
-void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
-{
-    BLE&        ble   = params->ble;
-    ble_error_t error = params->error;
-
-    if (error != BLE_ERROR_NONE) {
-        onBleInitError(ble, error);
-        return;
-    }
-
-    if (ble.getInstanceID() != BLE::DEFAULT_INSTANCE) {
-        return;
-    }
-
-    ble.gap().onDisconnection(disconnectionCallback);
-
-    /* Setup primary service. */
-    thermometerServicePtr = new HealthThermometerService(ble, currentTemperature, HealthThermometerService::LOCATION_EAR);
-
-    /* setup advertising */
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list));
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::THERMOMETER_EAR);
-    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
-    ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
-    ble.gap().setAdvertisingInterval(1000); /* 1000ms */
-    ble.gap().startAdvertising();
-}
-
-void scheduleBleEventsProcessing(BLE::OnEventsToProcessCallbackContext* context) {
-    BLE &ble = BLE::Instance();
-    eventQueue.call(Callback<void()>(&ble, &BLE::processEvents));
-}
+//#define THERMOMETER DS18S20
+#define THERMOMETER DS18B20
 
 int main()
 {
-    eventQueue.call_every(500, periodicCallback);
 
-    BLE &ble = BLE::Instance();
-    ble.onEventsToProcess(scheduleBleEventsProcessing);
-    ble.init(bleInitComplete);
-
-    eventQueue.dispatch_forever();
-
-    return 0;
+    // device( crcOn, useAddress, parasitic, mbed pin )
+    THERMOMETER device(true, false, false, D2);
+    
+    while (!device.initialize());    // keep calling until it works
+    
+    while (true)
+    {
+        // changing the resolutions only affects the DS18B20. The DS18S20 is fixed.
+        device.setResolution(nineBit);
+        device.readTemperature(); 
+        wait(2);
+        device.setResolution(tenBit);
+        device.readTemperature(); 
+        wait(2);
+        device.setResolution(elevenBit);
+        device.readTemperature(); 
+        wait(2);
+        device.setResolution(twelveBit);
+        device.readTemperature(); 
+        wait(2);
+    }
+     
+    return EXIT_SUCCESS;
 }
